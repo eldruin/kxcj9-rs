85a0bc1424ed9c823e68e3beaafced1bcc1d9a0e
Diego Barrios Romero <eldruin@gmail.com>
2019-04-13 11:12:12 +0200

    Extract functions

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,10 +1,18 @@
 extern crate embedded_hal_mock as hal;
 extern crate kxcj9;
-use hal::i2c::Mock as I2cMock;
+use hal::i2c::{Mock as I2cMock, Transaction as I2cTrans};
 use kxcj9::{Kxcj9, SlaveAddr};
+
+pub fn new(transactions: &[I2cTrans]) -> Kxcj9<I2cMock> {
+    Kxcj9::new(I2cMock::new(&transactions), SlaveAddr::default())
+}
+
+pub fn destroy(sensor: Kxcj9<I2cMock>) {
+    sensor.destroy().done();
+}
 
 #[test]
 fn can_create_and_destroy() {
-    let sensor = Kxcj9::new(I2cMock::new(&[]), SlaveAddr::default());
-    sensor.destroy().done();
+    let sensor = new(&[]);
+    destroy(sensor);
 }

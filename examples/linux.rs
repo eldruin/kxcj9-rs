548617bc0352468829adcf4b0d47f0be83b05b29
Diego Barrios Romero <eldruin@gmail.com>
2019-04-14 20:50:18 +0200

    Enable sensor

--- examples/linux.rs
+++ examples/linux.rs
@@ -4,8 +4,9 @@ use kxcj9::{Kxcj9, SlaveAddr};
 
 fn main() {
     let dev = hal::I2cdev::new("/dev/i2c-1").unwrap();
-    let address =  SlaveAddr::default();
+    let address = SlaveAddr::default();
     let mut sensor = Kxcj9::new_1018(dev, address);
+    sensor.enable().unwrap();
     let acc = sensor.read().unwrap();
     println!("X: {:2}, Y: {:2}, Z: {:2}", acc.x, acc.y, acc.z);
 }
